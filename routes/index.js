4f2db9a5c72e42e7283e713f04a9f152c48f7b6a
Matthew Thorley <padwasabimasala on gmail>
2014-06-04 19:27:32 -0600

    add score

--- routes/index.js
+++ routes/index.js
@@ -16,25 +16,41 @@ exports.init = function(app) {
 
 */
 
+var number_right = 0;
+var number_wrong = 0;
 
 index = function(req, res){
-var first = parseInt(req.query.firstAddend)
-var second = parseInt(req.query.secondAddend)
-var answer = parseInt(req.query.answer)
-var right_wrong = 'right'
 
-// if (first + second == answer) {
-//   res.send("Your answer was correct")
-// } else {
-//   res.send("Your answer was incorrect")
-// }
-
-res.render('index', {
-    title: 'Livy\'s App',
-    message: 'Boo hoo. Wah wah wah',
-    right_wrong: right_wrong,
-    name: 'Livy Lu Li Bu',
-    firstAddend: Math.round(Math.random() * 10) ,
-    secondAddend: Math.round(Math.random() * 10)
-  });
+	var first = parseInt(req.query.firstAddend)
+	var second = parseInt(req.query.secondAddend)
+	var answer = parseInt(req.query.answer)
+	var right_wrong = 'unset'
+	
+	if (req.query.answer != null) {
+		if (first + second == answer) {
+		  right_wrong = 'right'
+		  number_right++;
+		} else {
+		  right_wrong = 'wrong'
+		  number_wrong++;
+		}
+	}
+	
+	if (req.query.newGame) { 		
+		number_right = 0;
+		number_wrong = 0;
+	}
+	
+	var done = number_right + number_wrong;	
+	
+	
+	res.render('index', {
+		title: 'Livy\'s App',
+		number_right: number_right,
+		number_wrong: number_wrong,
+		right_wrong: right_wrong,
+		firstAddend: Math.round(Math.random() * 10) ,
+		secondAddend: Math.round(Math.random() * 10),
+	    done: done
+	});
 };
